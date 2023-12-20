4df1f458a5a18f9d7e923ceee9235b655061ea2a
Chris Prater <>
2023-12-20 10:46:14 -0700

    Share context with different providers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const initialState = { message: 'initial', setMessage: () => {} };
+const MyContext = React.createContext(initialState);
+
+const Message = () => {
+  const { message, setMessage } = React.useContext(MyContext);
+  const [ textVal, setTextVal ] = React.useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <p>{message}</p>
+      <input type="text" onChange={(e) => setTextVal(e.target.value)} />
+      <button
+        onClick={() => setMessage(textVal)}
+      >
+        Change
+      </button>
+    </>
+  )
+}
+
+const useProviderValue = () => {
+  const [message, setMessage] = React.useState(initialState.message);
+
+  window.providerValue = {
+    message,
+    setMessage,
+  };
+
+  return window.providerValue;
+}
+
+export function App(props) {
+  const providerValue = useProviderValue();
+
+  console.log('provider', providerValue);
+
+  return (
+    <div className='App'>
+      <h1>Hello React.</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <MyContext.Provider value={providerValue}>
+        <Message />
+      </MyContext.Provider>
+      <MyContext.Provider value={providerValue}>
+        <Message />
+      </MyContext.Provider>
     </div>
   );
 }
-
-export default App;
